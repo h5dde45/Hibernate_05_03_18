f899015204c677bba2be3391e8136046685f6d61
tmvf <tmvf@yandex.ru>
2018-03-05 20:34:27 +0300

    izm_2

--- src/main/java/com/hibernatetest/main/Main.java
+++ src/main/java/com/hibernatetest/main/Main.java
@@ -1,10 +1,12 @@
 package com.hibernatetest.main;
 
-import com.hibernatetest.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,13 +17,16 @@ public class Main {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
 
-        Student student = session.get(Student.class,7);
-        student.setAge(34);
+//        Student student = session.get(Student.class,1);
+//        Set<Exam> exams = student.getExams();
 //        System.out.println(student);
 //        student.setFirstname("fn1");
 //        student.setLastname("ln1");
 //        student.setAge(111);
 //        session.save(student);
+
+        Query query = session.createQuery("from Student ");
+        List resultList = query.getResultList();
         transaction.commit();
 
         session.close();
